4f0ef5cfb438acc42fe9a779ec5cfcd7ded68bd2
Tiago <djtiago29@gmail.com>
2024-11-26 21:37:11 +0000

    Add new activity

--- app/src/main/java/pt/tiago/mylogin/MainActivity.kt
+++ app/src/main/java/pt/tiago/mylogin/MainActivity.kt
@@ -1,16 +1,22 @@
 package pt.tiago.mylogin
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        //Ola
+
+        // Referência ao botão
+        val button = findViewById<Button>(R.id.button)
+        // Configurando o clique no botão
+        button.setOnClickListener {
+            // Criando um intent para abrir a SecondActivity
+            val intent = Intent(this@MainActivity, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
